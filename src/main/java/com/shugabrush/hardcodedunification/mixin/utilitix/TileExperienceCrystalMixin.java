f46ccd6ec7df53b4cecabd68f939e5f8b3b01d62
Shugabrush <alexwood319@gmail.com>
2026-04-26 10:29:03 -0700

    Got experience unified for UtilitiX

--- src/main/java/com/shugabrush/hardcodedunification/mixin/utilitix/TileExperienceCrystalMixin.java
+++ src/main/java/com/shugabrush/hardcodedunification/mixin/utilitix/TileExperienceCrystalMixin.java
@@ -1,4 +1,19 @@
 package com.shugabrush.hardcodedunification.mixin.utilitix;
 
+import com.shugabrush.hardcodedunification.utils.FluidUnification;
+import de.melanx.utilitix.content.experiencecrystal.TileExperienceCrystal;
+import net.minecraftforge.fluids.FluidStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = TileExperienceCrystal.class, remap = false)
 public class TileExperienceCrystalMixin
-{}
+{
+    @Inject(method = "getFluidInTank", at = @At("RETURN"), cancellable = true)
+    void getUnifiedFluidInTank(int tank, CallbackInfoReturnable<FluidStack> cir)
+    {
+        cir.setReturnValue(FluidUnification.getFluid(cir.getReturnValue()));
+    }
+}
