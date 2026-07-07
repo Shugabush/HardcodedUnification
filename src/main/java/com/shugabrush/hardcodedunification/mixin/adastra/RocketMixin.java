039b85b901435681f93f1c266c06c4af51113d65
Shugabrush <alexwood319@gmail.com>
2026-07-07 15:15:07 -0700

    Got AE2 and some addons in

--- src/main/java/com/shugabrush/hardcodedunification/mixin/adastra/RocketMixin.java
+++ src/main/java/com/shugabrush/hardcodedunification/mixin/adastra/RocketMixin.java
@@ -1,9 +1,10 @@
 package com.shugabrush.hardcodedunification.mixin.adastra;
 
-import com.shugabrush.hardcodedunification.utils.FluidUnification;
-import earth.terrarium.adastra.common.entities.vehicles.Rocket;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+
+import com.shugabrush.hardcodedunification.utils.FluidUnification;
+import earth.terrarium.adastra.common.entities.vehicles.Rocket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -11,7 +12,10 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(value = Rocket.class, remap = false)
 public class RocketMixin
 {
-    @ModifyArg(method = "fluid", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/DefaultedRegistry;get(Lnet/minecraft/resources/ResourceLocation;)Ljava/lang/Object;"))
+
+    @ModifyArg(method = "fluid",
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/core/DefaultedRegistry;get(Lnet/minecraft/resources/ResourceLocation;)Ljava/lang/Object;"))
     public ResourceLocation fluid(ResourceLocation resourceLocation)
     {
         return BuiltInRegistries.FLUID.getKey(FluidUnification.getFluid(resourceLocation));
